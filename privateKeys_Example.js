890a57367dd67c72f8fcc74ce58b37cac73cbdcf
fernandofilipe <fernando.filipereis@gmail.com>
2023-08-04 10:50:45 -0300

    atualizando gitignore

---
+++ privateKeys_Example.js
@@ -0,0 +1,4 @@
+const token =
+  "sl.BjerZD2nrr_Wh1eUK_-rAcM1_WSprwry1FCP1rM_cXVE03kp1CVD_Q2z5jj1YGdVTvmOTPySu44uxcYrME7RlNWxudPnumdC8RdDKKMPdMRWWRn36g2FuY6bRFNkK8-7PB-rvZqH7ZVEwxk";
+
+module.exports = token;
--- settings.js
+++ settings.js
@@ -1,10 +1,9 @@
-const token =
-  "sl.BjerZD2nrr_Wh1eUK_-rAcM1_WSprwry1FCP1rM_cXVE03kp1CVD_Q2z5jj1YGdVTvmOTPySu44uxcYrME7RlNWxudPnumdC8RdDKKMPdMRWWRn36g2FuY6bRFNkK8-7PB-rvZqH7ZVEwxk";
+
 
 const settings = {
   token: token,
   file: {
-    path: "",
+    path: "./output/",
     name: "multitrack",
   },
   csv: {
